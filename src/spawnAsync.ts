583b7a8a5dc7eb8fe7d7c7f18a49ea2a05a82b5c
Phil Pluckthun <phil@kitten.sh>
2026-05-15 09:56:31 +0100

    Enforce maxBuffer with default limit
    - Unset means we enforce this limit lazily
    - Set means we enforce it eagerly
    - Delay buffer concat until evaluation time

--- src/spawnAsync.ts
+++ src/spawnAsync.ts
@@ -1,9 +1,11 @@
 import { ChildProcess, SpawnOptions as NodeSpawnOptions } from 'child_process';
+import { constants as bufferConstants } from 'buffer';
 import spawn from 'cross-spawn';
 
 namespace spawnAsync {
   export interface SpawnOptions extends NodeSpawnOptions {
     ignoreStdio?: boolean;
+    maxBuffer?: number;
   }
 
   export interface SpawnPromise<T> extends Promise<T> {
@@ -20,6 +22,13 @@ namespace spawnAsync {
   }
 }
 
+type IOChunk = string | Buffer;
+
+interface IOChunksState {
+  buffer: IOChunk[];
+  maxExceeded: boolean;
+}
+
 function spawnAsync(
   command: string,
   args?: ReadonlyArray<string>,
@@ -30,72 +39,152 @@ function spawnAsync(
 
   let child: ChildProcess;
   let promise = new Promise((resolve, reject) => {
-    let { ignoreStdio, ...nodeOptions } = options;
+    let {
+      ignoreStdio,
+      maxBuffer = bufferConstants.MAX_STRING_LENGTH,
+      ...nodeOptions
+    } = options;
     child = spawn(command, args, nodeOptions);
-    let stdout = '';
-    let stderr = '';
+
+    // NOTE(@kitten): When `maxBuffer` is set explicitly, we enforce it strictly
+    // and don't produce a result without it being strictly enforced
+    const enforceMaxBufferStrictly = options.maxBuffer != null;
+
+    const stdoutChunks: IOChunksState = { buffer: [], maxExceeded: false };
+    const stderrChunks: IOChunksState = { buffer: [], maxExceeded: false };
+
+    function makeHandler(chunks: IOChunksState) {
+      let length = 0;
+      return (chunk: IOChunk) => {
+        chunks.buffer.push(chunk);
+        length += typeof chunk === 'string' ? Buffer.byteLength(chunk) : chunk.byteLength;
+        while (chunks.buffer.length > 0 && length > maxBuffer) {
+          chunks.maxExceeded = true;
+          chunk = chunks.buffer[0];
+          const chunkLength = typeof chunk === 'string' ? Buffer.byteLength(chunk) : chunk.byteLength;
+          if (length - chunkLength <= maxBuffer) {
+            const replacement = typeof chunk === 'string' ? Buffer.from(chunk) : chunk;
+            const excess = length - maxBuffer;
+            chunks.buffer[0] = replacement.subarray(excess);
+            length -= excess;
+            break;
+          } else {
+            chunks.buffer.shift();
+            length -= chunkLength;
+          }
+        }
+      };
+    }
+
+    function attachResult<T extends spawnAsync.SpawnResult | (Error & Partial<spawnAsync.SpawnResult>)>(
+      target: T,
+      assign: Partial<spawnAsync.SpawnResult>,
+      stdoutChunks: IOChunksState,
+      stderrChunks: IOChunksState,
+      skipMaxBufferCheck?: boolean,
+    ): T {
+      function makeMaxBufferError() {
+        const argumentString = args && args.length > 0 ? ` ${args.join(' ')}` : '';
+        const error: Error & { code?: string } = new Error(`${command}${argumentString} exceeded maxBuffer of ${maxBuffer} bytes`);
+        error.code = 'ERR_CHILD_PROCESS_STDIO_MAXBUFFER';
+        return attachResult(error, assign, stdoutChunks, stderrChunks, true);
+      }
+
+      let _stdout: string | undefined;
+      let _stderr: string | undefined;
+      const map: PropertyDescriptorMap = {
+        stdout: {
+          enumerable: true,
+          configurable: true,
+          get() {
+            if (!skipMaxBufferCheck && stdoutChunks.maxExceeded) {
+              throw makeMaxBufferError();
+            } else if (_stdout === undefined) {
+              _stdout = Buffer.concat(
+                stdoutChunks.buffer.map((chunk) => typeof chunk === 'string' ? Buffer.from(chunk) : chunk)
+              ).toString('utf8');
+            }
+            return _stdout;
+          },
+        },
+        stderr: {
+          enumerable: true,
+          configurable: true,
+          get() {
+            if (!skipMaxBufferCheck && stderrChunks.maxExceeded) {
+              throw makeMaxBufferError();
+            } else if (_stderr === undefined) {
+              _stderr = Buffer.concat(
+                stderrChunks.buffer.map((chunk) => typeof chunk === 'string' ? Buffer.from(chunk) : chunk)
+              ).toString('utf8');
+            }
+            return _stderr;
+          },
+        },
+        output: {
+          enumerable: true,
+          configurable: true,
+          get: () => [target.stdout, target.stderr],
+        },
+      };
+      for (const key in assign) {
+        map[key] = {
+          value: assign[key as keyof spawnAsync.SpawnResult],
+          enumerable: true,
+          writable: true,
+          configurable: true,
+        };
+      }
+      Object.defineProperties(target, map);
+      return target;
+    }
 
     if (!ignoreStdio) {
-      if (child.stdout) {
-        child.stdout.on('data', (data) => {
-          stdout += data;
-        });
-      }
-
-      if (child.stderr) {
-        child.stderr.on('data', (data) => {
-          stderr += data;
-        });
-      }
+      child.stdout?.on('data', makeHandler(stdoutChunks));
+      child.stderr?.on('data', makeHandler(stderrChunks));
     }
 
     let completionListener = (code: number | null, signal: string | null) => {
       child.removeListener('error', errorListener);
-      let result: spawnAsync.SpawnResult = {
+      const argumentString = args && args.length > 0 ? ` ${args.join(' ')}` : '';
+      let error: (Error & { code?: string }) | null = null;
+      if (code !== 0) {
+        error = signal
+          ? new Error(`${command}${argumentString} exited with signal: ${signal}`)
+          : new Error(`${command}${argumentString} exited with non-zero code: ${code}`);
+      }
+      const assignResult: Partial<spawnAsync.SpawnResult> = {
         pid: child.pid,
-        output: [stdout, stderr],
-        stdout,
-        stderr,
         status: code,
         signal,
       };
-      if (code !== 0) {
-        let argumentString = args && args.length > 0 ? ` ${args.join(' ')}` : '';
-        let error = signal
-          ? new Error(`${command}${argumentString} exited with signal: ${signal}`)
-          : new Error(`${command}${argumentString} exited with non-zero code: ${code}`);
-        if (error.stack && callerStack) {
-          error.stack += `\n${callerStack}`;
-        }
-        Object.assign(error, result);
-        reject(error);
+      if (error) {
+        if (error.stack && callerStack) error.stack += `\n${callerStack}`;
+        // When we're already rejecting, we don't enforce the max buffer error, and accept that we
+        // may truncate stderr/stdout
+        reject(attachResult(error, assignResult, stdoutChunks, stderrChunks, true));
+      } else if (enforceMaxBufferStrictly && (stdoutChunks.maxExceeded || stderrChunks.maxExceeded)) {
+        // When a `maxBuffer` is passed, we enforce the maximum on stdout and stderr strictly
+        const error: Error & { code?: string } = new Error(`${command}${argumentString} exceeded maxBuffer of ${maxBuffer} bytes`);
+        error.code = 'ERR_CHILD_PROCESS_STDIO_MAXBUFFER';
+        reject(attachResult(error, assignResult, stdoutChunks, stderrChunks, true));
       } else {
-        resolve(result);
+        const result = {} as spawnAsync.SpawnResult;
+        resolve(attachResult(result, assignResult, stdoutChunks, stderrChunks));
       }
     };
 
     let errorListener = (error: Error) => {
-      if (ignoreStdio) {
-        child.removeListener('exit', completionListener);
-      } else {
-        child.removeListener('close', completionListener);
-      }
-      Object.assign(error, {
+      child.removeListener(ignoreStdio ? 'exit' : 'close', completionListener);
+      const assignResult: Partial<spawnAsync.SpawnResult> = {
         pid: child.pid,
-        output: [stdout, stderr],
-        stdout,
-        stderr,
         status: null,
         signal: null,
-      });
-      reject(error);
+      };
+      reject(attachResult(error, assignResult, stdoutChunks, stderrChunks));
     };
 
-    if (ignoreStdio) {
-      child.once('exit', completionListener);
-    } else {
-      child.once('close', completionListener);
-    }
+    child.once(ignoreStdio ? 'exit' : 'close', completionListener);
     child.once('error', errorListener);
   }) as spawnAsync.SpawnPromise<spawnAsync.SpawnResult>;
 
